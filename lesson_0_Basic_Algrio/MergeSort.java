dfb09ce625451cc0b235bb7157155cd517f36c50
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-06-30 19:59:41 +0800

    classific algorithm

--- lesson_0_Basic_Algrio/MergeSort.java
+++ lesson_0_Basic_Algrio/MergeSort.java
@@ -1,4 +1,44 @@
 package leetcode.lesson_0_Basic_Algrio;
 
+import java.util.Arrays;
+
 public class MergeSort {
+    public int[] aux;
+
+    public int[] msort(int[] nums){
+        aux = new int[nums.length];
+
+        mergesort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public void mergesort(int[] nums, int lo, int hi){
+        if (hi <= lo) return;
+        int mid = (hi - lo)/2 + lo;
+        mergesort(nums, lo, mid);
+        mergesort(nums, mid + 1, hi);
+        merge(nums, lo, mid, hi);
+    }
+
+    public void merge(int[] nums, int lo, int mid, int hi){
+        // the for iterative here
+        for (int i = 0; i < nums.length; i ++){
+            aux[i] = nums[i];
+        }
+       int i = lo, j = mid + 1;
+
+       for (int k = lo; k <= hi; k ++){
+           if (i > mid) nums[k] = aux[j++];
+           else if (j > hi) nums[k] = aux[i++];
+           else if (aux[i] > aux[j]) nums[k] = aux[j++];
+           else nums[k]  = aux[i ++];
+       }
+    }
+
+    public static void main(String[] args){
+        MergeSort ms = new MergeSort();
+
+        int[] nums = {14, 8, 5, 4, 9};
+        System.out.println(Arrays.toString(ms.msort(nums)));
+    }
 }
--- lesson_0_Basic_Algrio/QuickSort.java
+++ lesson_0_Basic_Algrio/QuickSort.java
@@ -1,4 +1,42 @@
 package leetcode.lesson_0_Basic_Algrio;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public void exch(int[] nums, int a, int b){
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
+    }
+
+    public int[] qsort(int[] nums){
+        quicksort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public void quicksort(int[] nums, int lo, int hi){
+        if (lo >= hi) return;
+        int k = partition(nums, lo, hi);
+        quicksort(nums, lo, k - 1);
+        quicksort(nums, k + 1, hi);
+    }
+
+    public int partition(int[] nums, int lo, int hi){
+        int v = nums[lo];
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (nums[++ i] <= v) if (i >= hi) break;
+            while (nums[-- j] >= v) if (j <= lo) break;
+            if (i >= j) break;
+            exch(nums, i, j);
+        }
+        exch(nums, lo, j);
+        return --i;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {9,8,7,2,4,5,6,10};
+        QuickSort qs = new QuickSort();
+        System.out.println(Arrays.toString(qs.qsort(nums)));
+    }
 }
