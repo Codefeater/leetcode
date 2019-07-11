a6ae5dbd47cc476450d4509419f51b43dad8b87e
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-11 23:18:01 +0800

    dot fix

--- lesson_6_RecursiveAndBacktracking/Combinations.java
+++ lesson_6_RecursiveAndBacktracking/Combinations.java
@@ -7,25 +7,19 @@ public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> aux = new ArrayList<>();
-
         findCombinations(n, ans, aux,0, k);
-
         return ans;
     }
 
     public void findCombinations(int n, List<List<Integer>> ans, List<Integer> aux, int index, int k){
-
         if (aux.size() == k){
             ans.add(new ArrayList<>(aux));
             return;
         }
-
         for (int i = index; i < n; i ++){
-
             aux.add(i + 1);
             findCombinations(n, ans, aux, i + 1, k);
             aux.remove(new Integer(i + 1));
-
             }
         }
 
