0b22b5684148a1d267b69d050973fc4f8d6131b4
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-11 09:33:24 +0800

    bit compute

---
+++ lesson_0_Basic_Algrio/SingleNumberIII_260.java
@@ -0,0 +1,32 @@
+package leetcode.lesson_0_Basic_Algrio;
+
+public class SingleNumberIII_260 {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for(int num : nums) diff ^= num;
+
+        int indexOf = getIndex(diff);
+
+        int num1 = 0, num2 = 0;
+
+        for (int j = 0; j < nums.length; ++j)
+            if (Isbit(nums[j], indexOf)) num1 ^= nums[j];
+            else num2 ^= nums[j];
+
+        return new int[]{num1, num2};
+    }
+
+    public int getIndex(int miniArr){
+        int indexBit = 0;
+        while ((miniArr & 1) == 0){
+            miniArr = miniArr >> 1;
+            indexBit ++;
+        }
+        return indexBit;
+    }
+
+    public boolean Isbit(int num, int Index){
+        num = num >> Index;
+        return (num & 1) == 1 ? true : false;
+    }
+}
