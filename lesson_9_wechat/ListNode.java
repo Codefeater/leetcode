08d54b63d635a397e20c4aaf01d07dc8d20388dc
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-20 18:30:26 +0800

    tidy it

--- lesson_9_wechat/ListNode.java
+++
@@ -1,7 +0,0 @@
-package leetcode.lesson_9_wechat;
-
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
-}
--- lesson_9_wechat/TreeNode.java
+++
@@ -1,9 +0,0 @@
-package leetcode.lesson_9_wechat;
-
-public class TreeNode
-{
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
