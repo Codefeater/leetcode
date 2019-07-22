add705429e3acf8fcff29470ba344f2c5c4f1629
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-22 09:42:31 +0800

    min_depth

---
+++ lesson_5_binaryTree/minDepth_111.java
@@ -0,0 +1,18 @@
+package leetcode.lesson_5_binaryTree;
+
+public class minDepth_111 {
+    public int minDepth(TreeNode root) {
+        int ans = assist(root);
+        return ans;
+    }
+
+    public int assist(TreeNode root){
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) min_depth = Math.min(min_depth, assist(root.left));
+        if (root.right != null) min_depth = Math.min(min_depth, assist(root.right));
+        return min_depth + 1;
+    }
+}
