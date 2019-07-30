4b49ff98815fbf0d49456515852d48344c15e279
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2019-07-30 09:23:29 +0800

    add recursive reverselist

--- lesson_3_linkedlist/RevreseLinkedList_206.java
+++ lesson_3_linkedlist/RevreseLinkedList_206.java
@@ -1,6 +1,6 @@
 package leetcode.lesson_3_linkedlist;
 
-public class RevreseLinkedList {
+public class RevreseLinkedList_206 {
         public ListNode reverseList(ListNode head) {
             ListNode pre = null;
             ListNode cur = head;
@@ -12,4 +12,13 @@ public class RevreseLinkedList {
             }
             return pre;
         }
+
+        public ListNode reverseListRecur(ListNode head){
+            if (head == null || head.next == null) return head;
+            ListNode p = reverseListRecur(head.next);
+            head.next.next = head;
+            head.next = p;
+            return p;
+        }
+
 }
