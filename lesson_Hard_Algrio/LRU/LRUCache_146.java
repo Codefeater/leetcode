07a6f57ace5074b492c20ebc040a12e4e4da9c4d
倪飞翔 <nifeixiang@nifeixiangdeMBP.lan>
2020-03-28 19:41:37 +0800

    LRU && Trie
    Signed-off-by: 2643104428@qq.com <nokialumia36>

---
+++ lesson_Hard_Algrio/LRU/LRUCache_146.java
@@ -0,0 +1,39 @@
+package leetcode.lesson_Hard_Algrio.LRU;
+
+import java.util.HashMap;
+
+public class LRUCache_146 {
+    //    core idea: map(k, node), doubleList(node)
+    private HashMap<Integer, Node> map;
+    private DoubleList cache;
+    private int cap;
+
+    public LRUCache_146(int capacity) {
+        this.cap = capacity;
+        map = new HashMap<>();
+        cache = new DoubleList();
+    }
+
+    //    core part1
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        int val = map.get(key).val;
+        put(key, val);
+        return val;
+    }
+
+    //    core part2
+    public void put(int key, int val) {
+        Node x = new Node(key, val);
+        if (map.containsKey(key)) {
+            cache.remove(map.get(key));
+        } else {
+            if (cap == cache.size()) {
+                Node last = cache.removeLast();
+                map.remove(last.key);
+            }
+        }
+        cache.addFirst(x);
+        map.put(key, x);
+    }
+}
