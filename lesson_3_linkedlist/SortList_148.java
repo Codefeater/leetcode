02075563b6eefaa26203ae33e609811f2a9f0d33
倪飞翔 <nifeixiang@nifeixiangdemacbook-pro.local>
2020-03-29 17:16:56 +0800

    optimize the mergeList code
    Signed-off-by: 2643104428@qq.com <nokialumia36>

---
+++ lesson_3_linkedlist/SortList_148.java
@@ -0,0 +1,37 @@
+package leetcode.lesson_3_linkedlist;
+
+public class SortList_148 {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+//        直接设置2个节点，作为快慢跑的节点，归并的2列开始分出
+        ListNode prev = null, slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode p = dummyNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
+        return dummyNode.next;
+    }
+}
--- lesson_3_linkedlist/mergeTwoLists_21.java
+++ lesson_3_linkedlist/mergeTwoLists_21.java
@@ -2,29 +2,20 @@ package leetcode.lesson_3_linkedlist;
 
 public class mergeTwoLists_21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
         ListNode dummyNode = new ListNode(0);
-        ListNode cur = dummyNode;
-        while (l1 != null || l2 != null) {
-            if (l1 == null) {
-                cur.next = l2;
-                l2 = l2.next;
-                cur = cur.next;
-            } else if (l2 == null) {
-                cur.next = l1;
+        ListNode p = dummyNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
                 l1 = l1.next;
-                cur = cur.next;
-            } else if (l1.val < l2.val) {
-                cur.next = l1;
-                l1 = l1.next;
-                cur = cur.next;
             } else {
-                cur.next = l2;
+                p.next = l2;
                 l2 = l2.next;
-                cur = cur.next;
             }
+            p = p.next;
         }
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
         return dummyNode.next;
     }
 }
