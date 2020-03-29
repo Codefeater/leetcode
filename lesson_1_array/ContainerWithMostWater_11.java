3995999cbac929b07bba8124c1fa73794e969a22
2643104428@qq.com <nokialumia36>
2020-03-29 22:49:50 +0800

    max reaa -- water
    Signed-off-by: 2643104428@qq.com <nokialumia36>

---
+++ lesson_1_array/ContainerWithMostWater_11.java
@@ -0,0 +1,14 @@
+package leetcode.lesson_1_array;
+
+public class ContainerWithMostWater_11 {
+    public int maxArea(int[] height) {
+        int area = 0, lo = 0, hi = height.length - 1, min_height = 0;
+        while (lo < hi) {
+            min_height = Math.min(height[lo], height[hi]);
+            area = Math.max(area, min_height * (hi - lo));
+            if (height[lo] < height[hi]) lo++;
+            else hi--;
+        }
+        return area;
+    }
+}
