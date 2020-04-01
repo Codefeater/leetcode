efde3a32cee27add2a2eeebedd0743b3dac3a58e
2643104428@qq.com <nokialumia36>
2020-04-01 19:16:04 +0800

    post order
    Signed-off-by: 2643104428@qq.com <nokialumia36>

--- lesson_4_qstack/PostOrder_Iterative.java
+++ lesson_4_qstack/PostOrder_Iterative.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class PostOrder_Iterative {
+
     List<Integer> res = new ArrayList<>();
 
     public static void assist(TreeNode root, List<Integer> res) {
@@ -14,26 +15,28 @@ public class PostOrder_Iterative {
         res.add(root.val);
     }
 
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<Integer>();
-        if (root == null) return ans;
-        Stack<TreeNode> sk = new Stack<TreeNode>();
+//    Recursive
+public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> ans = new ArrayList<Integer>();
+    if (root == null) return ans;
+    Stack<TreeNode> sk = new Stack<TreeNode>();
 
-        sk.push(root);
-        while (!sk.empty()) {
-            TreeNode aux = sk.peek();
-            if (aux.left == null && aux.right == null) {
-                ans.add(sk.pop().val);
-            } else {
-                if (aux.right != null) sk.push(aux.right);
-                if (aux.left != null) sk.push(aux.left);
-                aux.right = null;
-                aux.left = null;
-            }
+    sk.push(root);
+    while (!sk.empty()) {
+        TreeNode aux = sk.peek();
+        if (aux.left == null && aux.right == null) {
+            ans.add(sk.pop().val);
+        } else {
+            if (aux.right != null) sk.push(aux.right);
+            if (aux.left != null) sk.push(aux.left);
+            aux.right = null;
+            aux.left = null;
         }
-        return ans;
     }
+    return ans;
+}
 
+    //    Iterative
     public List<Integer> posterIterative(TreeNode root) {
         assist(root, res);
         return res;
