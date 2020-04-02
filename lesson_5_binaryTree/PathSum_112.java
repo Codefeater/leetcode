8145dbad5f0ddc3a02552a21a8cc6c452064949c
2643104428@qq.com <nokialumia36>
2020-04-02 21:54:28 +0800

    format
    Signed-off-by: 2643104428@qq.com <nokialumia36>

--- lesson_5_binaryTree/PathSum_112.java
+++ lesson_5_binaryTree/PathSum_112.java
@@ -4,6 +4,7 @@ public class PathSum_112 {
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
         if (root.right == null && root.left == null) return root.val == sum;
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        return hasPathSum(root.left, sum - root.val)
+                || hasPathSum(root.right, sum - root.val);
     }
 }
