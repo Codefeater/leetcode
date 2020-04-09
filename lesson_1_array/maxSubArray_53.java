65c82d36b441271c2b9a842e91f712e34c7f945a
2643104428@qq.com <nokialumia36>
2020-04-09 22:04:09 +0800

    max sub array
    Signed-off-by: 2643104428@qq.com <nokialumia36>

---
+++ lesson_1_array/maxSubArray_53.java
@@ -0,0 +1,13 @@
+package leetcode.lesson_1_array;
+
+public class maxSubArray_53 {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) return -1;
+        int ans = Integer.MIN_VALUE, maxed = 0;
+        for (int i = 0; i < nums.length; i++) {
+            maxed = Math.max(maxed + nums[i], nums[i]);
+            ans = Math.max(ans, maxed);
+        }
+        return ans;
+    }
+}
